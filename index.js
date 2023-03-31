b4fb7edafbba3229a68ed1079ba889c267d659bc
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-03-30 22:59:37 -0400

    complete dom modifying

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// index.js
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
